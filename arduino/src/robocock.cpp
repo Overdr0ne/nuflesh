86793cfbaae5f3193d6acfb1ce63a3e15af4f3e3
sam <0v3rdr0n3@gmail.com>
2017-08-08 23:47:08 +0000

    restructured dirs to accommodate host code

--- arduino/src/robocock.cpp
+++ arduino/src/robocock.cpp
@@ -1,18 +1,10 @@
-// HashMap - Version: Latest 
-/*#include <HashMap.h>*/
 #include <Arduino.h>
 #include <Wire.h>
-
-// Keyboard - Version: Latest 
 #include <Keyboard.h>
-
-// Mouse - Version: Latest 
 #include <Mouse.h>
 
 // Define Pins
-
 const int startEmulation = 2;      // switch to turn on and off mouse emulation
-//const int toggleMouse = 2;    // input pin for the mouse left Button
 const int joystickX = A1;         // joystick X axis
 const int joystickY = A0;         // joystick Y axis
 
@@ -24,24 +16,13 @@ const int PURPLE = 4;
 const int GREEN = 5;
 
 const int N_KEYS = 6;
-const int N_LAYERS = 4;
-const int N_GESTURES = 12;
-const int GEST_LEN = 5;
 
 struct Kbd {
 	int pin[N_KEYS];
-	char outChar[N_LAYERS][N_KEYS];
+	char outChar[N_KEYS];
 	int curState[N_KEYS];
 	int prevState[N_KEYS];
 	bool send[N_KEYS];
-	char gestStr[GEST_LEN];
-	bool gestWrapped;
-	int gestHead;
-	int prevGestHead;
-	bool gestComplete;
-	char gestures[N_GESTURES][GEST_LEN];
-	/*HashType<char*,char> hashRawArray[N_GESTURES];*/
-	/*HashMap<char*,char> hashMap = HashMap<char*,char>( hashRawArray , N_GESTURES );*/
 };
 
 struct Kbd kbd;
@@ -52,11 +33,17 @@ int responseDelay = 5;        // response delay of the mouse, in ms
 int threshold = cursorSpeed/5;      // resting threshold
 int lowerThreshold = cursorSpeed/2;      // resting threshold
 int upperThreshold = 3*cursorSpeed/4;      // resting threshold
+unsigned int lowerJoyThreshold = 40;
 int center = cursorSpeed/2;         // resting position value
+int xCenter,yCenter;
+int position=-1;
+int prevPosition=-2;
+bool sendPos;
 
-unsigned int dtime;
-unsigned int startTime;
+unsigned int dtime,dtimeCstick;
+unsigned int startTime,startTimeCstick;
 unsigned int kbdResponseTime = 5;
+unsigned int cStickResponseTime = 1;
 
 boolean mouseMode = false;    // whether or not to control the mouse
 boolean kbdInit = true;
@@ -70,39 +57,20 @@ void setup() {
 	kbd.pin[RED] = 6;
 	kbd.pin[PURPLE] = 7;
 	kbd.pin[GREEN] = 8;
-	kbd.outChar[0][ORANGE] = 'g';
-	kbd.outChar[0][RED] = 'f';
-	kbd.outChar[0][GREEN] = 'd';
-	kbd.outChar[0][BLUE] = 's';
-	kbd.outChar[0][PURPLE] = 'a';
-	kbd.outChar[0][YELLOW] = KEY_LEFT_CTRL;
-	kbd.outChar[1][ORANGE] = 't';
-	kbd.outChar[1][RED] = 'r';
-	kbd.outChar[1][GREEN] = 'e';
-	kbd.outChar[1][BLUE] = 'w';
-	kbd.outChar[1][PURPLE] = 'q';
-	kbd.outChar[1][YELLOW] = KEY_LEFT_CTRL;
-	kbd.outChar[2][ORANGE] = 'b';
-	kbd.outChar[2][RED] = 'v';
-	kbd.outChar[2][GREEN] = 'c';
-	kbd.outChar[2][BLUE] = 'x';
-	kbd.outChar[2][PURPLE] = 'z';
-	kbd.outChar[2][YELLOW] = KEY_LEFT_CTRL;
+	kbd.outChar[ORANGE] = 'g';
+	kbd.outChar[RED] = KEY_LEFT_SHIFT;
+	kbd.outChar[GREEN] = KEY_LEFT_CTRL;
+	kbd.outChar[BLUE] = KEY_LEFT_ALT;
+	kbd.outChar[PURPLE] = KEY_LEFT_GUI;
+	kbd.outChar[YELLOW] = KEY_ESC;
 	for(int i=0; i<N_KEYS; i++) {
 		pinMode(kbd.pin[i], INPUT_PULLUP);
 		kbd.send[i] = false;
 		kbd.prevState[i] = HIGH;
 	}
 
-	/*kbd.gestures[0] = {0,0,0,0,1};*/
-
-	/*kbd.hashMap[0](kbd.gestures[0],'a');*/
-
-	for(int i=0; i<GEST_LEN; i++) {
-	  kbd.gestStr[i] = 0;
-	}
-	kbd.gestHead = 0;
-	kbd.prevGestHead = GEST_LEN-1;
+	xCenter = analogRead(A1);
+	yCenter = analogRead(A0);
 
 	pinMode(startEmulation, INPUT_PULLUP);   // the switch pin
 	//	pinMode(mouseLeftButton, INPUT_PULLUP);  // the left mouse button pin
@@ -112,28 +80,7 @@ void setup() {
 }
 
 //axis for joystick
-int readAxis(int thisAxis, int offset) {
-	// read the analog input
-	int reading = analogRead(thisAxis);
-//	Serial.println(reading);
-	reading = reading + offset;
-
-	// map the reading from the analog input range to the output range
-	reading = map(reading, 0, 1023, 0, cursorSpeed);
-
-	// if the output reading is outside the
-	// rest position threshold,  use it
-	int distance = reading - center;
-
-	if (abs(distance) < lowerThreshold) {
-		distance=0;
-	}
-
-	// return the distance for this axis
-	return distance;
-}
-
-int readGestPos(int thisAxis, int offset) {
+int readMouseAxis(int thisAxis, int offset) {
 	// read the analog input
 	int reading = analogRead(thisAxis);
 	reading = reading + offset;
@@ -155,8 +102,8 @@ int readGestPos(int thisAxis, int offset) {
 
 void doMouseMode() {
 	// read and scale the two axes
-	int xReading = readAxis(A1,10);
-	int yReading = readAxis(A0,0);
+	int xReading = readMouseAxis(A1,10);
+	int yReading = readMouseAxis(A0,0);
 
 	Mouse.move(xReading, yReading, 0); // (x, y, scroll mouse wheel)
 
@@ -200,115 +147,103 @@ void getKeypresses() {
 	}
 }
 
-void sendKeys(int layer) {
+void sendKeys() {
 	kbdInit = false;
 	startTime = millis();
-	for(int i=0; i<N_LAYERS; i++) {
-		for(int j=0; j<N_KEYS; j++) {
-			if(kbd.send[j]==true && (layer==i)) {
-				Keyboard.press(kbd.outChar[i][j]);
-				kbd.send[j] = false;
-			}
+	for(int j=0; j<N_KEYS; j++) {
+		if(kbd.send[j]==true) {
+			Keyboard.press(kbd.outChar[j]);
+			kbd.send[j] = false;
 		}
 	}
 	Keyboard.releaseAll();
 }
 
-int getLayer() {
-	int layer = 0;
-	int xReading = readAxis(A1,10);
-	int yReading = readAxis(A0,0);
+int readJoyAxis(int thisAxis, int offset, int center)
+{
+	// read the analog input
+	int reading = analogRead(thisAxis);
+	reading = reading + offset;
 
-	if(abs(xReading) > abs(yReading)) {
-		if(xReading > 0)
-			layer=1;
-		else if(xReading < 0)
-			layer=2;
-	}
-	else {
-		if(yReading > 0)
-			layer=3;
-		else if(yReading < 0)
-			layer=4;
+	// if the output reading is outside the
+	// rest position threshold,  use it
+	unsigned int distance = reading - center;
+
+	if (abs(distance) < lowerJoyThreshold) {
+		distance=0;
 	}
 
-	return layer;
+	// return the distance for this axis
+	return distance;
 }
 
-void doKbdMode() {
-	int layer = 0;
+void getPosition()
+{
+	int xReading = readJoyAxis(A1,30,xCenter);
+	int yReading = readJoyAxis(A0,0,yCenter);
 
-	layer = getLayer();
-
-	getKeypresses();
-
-	dtime = millis() - startTime;
-	if(kbdInit || (dtime > kbdResponseTime)) {
-		sendKeys(layer);
+	if(xReading==0 && yReading==0) {
+		position=-3;
+		sendPos = false;
 	}
-}
+	else
+		sendPos = true;
 
-int getPosition() {
-	int position = 0;
-	int xReading = readAxis(A1,10);
-	int yReading = readAxis(A0,0);
+	Serial.println("X:");
+	Serial.println(xReading);
+	Serial.println("Y:");
+	Serial.println(yReading);
+	//delay(100);
 
 	if(abs(xReading) >= abs(yReading)) {
 		if(xReading > 0)
-			position=1;
+			position=KEY_RIGHT_ARROW;
 		else if(xReading < 0)
-			position=2;
+			position=KEY_LEFT_ARROW;
 	}
-	else if(abs(xReading) <= abs(yReading)) {
+	else if(abs(xReading) < abs(yReading)) {
 		if(yReading > 0)
-			position=3;
+			position=KEY_DOWN_ARROW;
 		else if(yReading < 0)
-			position=4;
-	}
-	
-	return position;
-}
-
-void resetGesture() {
-  for(int i=0; i<GEST_LEN; i++) {
-	  kbd.gestStr[i] = 0;
-	}
-	kbd.gestHead = 0;
-	kbd.prevGestHead = GEST_LEN-1;
-}
-
-void updateGesture(int position) {
-	if((position==0) && (kbd.gestStr[kbd.prevGestHead]!=0)) {
-		kbd.gestComplete = true;
-	}
-	else if(position!=kbd.gestStr[kbd.prevGestHead]) {
-	  kbd.gestStr[kbd.gestHead] = position;
-		kbd.gestHead=(kbd.gestHead+1) % GEST_LEN;
-		kbd.prevGestHead=(kbd.prevGestHead+1) % GEST_LEN;
+			position=KEY_UP_ARROW;
 	}
 }
 
-void sendGesture (int position) {
-	if(kbd.gestStr[0] == 1 && kbd.gestStr[1] == 4)
-		Keyboard.press('a');
-
-  Keyboard.releaseAll();
+void sendPosition()
+{
+	if(position==prevPosition)
+		return;
+	prevPosition = position;
+	startTimeCstick = millis();
+	if(sendPos)
+		Keyboard.press(position);
+	Keyboard.release(position);
 }
 
-void doGestureMode() {
-	int position;
+void doKbdMode()
+{
+	HID_ hid;
+	unsigned char test[3];
+	test[0]='a';
+	test[1]='b';
+	test[2]='c';
 
-	position = getPosition();
+	getKeypresses();
+	getPosition();
 
-	updateGesture(position);
-	if(kbd.gestComplete) {
-		sendGesture(position);
-		resetGesture();
-		kbd.gestComplete = false;
+	dtime = millis() - startTime;
+	if(kbdInit || (dtime > kbdResponseTime)) {
+		sendKeys();
 	}
+	dtimeCstick = millis() - startTimeCstick;
+	if(kbdInit || (dtimeCstick > cStickResponseTime)) {
+		sendPosition();
+	}
+	hid.SendReport(123,(void *)test,3);
 }
 
-void loop() {
+void loop()
+{
 	// read the switch:
 	int modeToggle = digitalRead(startEmulation);
 
@@ -327,7 +262,6 @@ void loop() {
 		delay(responseDelay);
 	}
 	else {
-		//		doKbdMode();
-		doGestureMode();
+		doKbdMode();
 	}
 }
